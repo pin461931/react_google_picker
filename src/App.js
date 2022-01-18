04afb27866e293c8d9e6c2d84052899fb1bb5dc0
pin461931 <pin46193@gmail.com>
2022-01-18 15:17:51 +0700

    add picker code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+/* global gapi */
+/* global google */
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const scope = ['https://www.googleapis.com/auth/drive.file'];
+  const APP_ID = process.env.REACT_APP_GOOGLE_APP_ID;
+  const CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+  const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+
+  const [oauthToken, setOauthToken] = useState("");
+  const [pickerApiLoaded, setPickerApiLoaded] = useState(false);
+
+  const handleAuth = () => {
+    gapi.load('auth', onAuthApiLoad);
+    gapi.load('picker', onPickerApiLoad);
+  }
+
+  const onAuthApiLoad = () => {
+    gapi.auth.authorize(
+      {
+        'client_id': CLIENT_ID,
+        'scope': scope,
+        'immediate': false
+      },
+      handleAuthResult);
+  }
+
+  const handleAuthResult = (authResult) => {
+    if (authResult && !authResult.error) {
+      setOauthToken(authResult.access_token);
+      createPicker();
+    }
+  }
+
+  const pickerCallback = (data) => {
+    if (data.action === window.google.picker.Action.PICKED) {
+      const fileId = data.docs[0].id;
+      alert('The user selected: ' + fileId);
+    }
+  }
+
+  const createPicker = () => {
+    if (pickerApiLoaded && oauthToken) {
+      const view = new google.picker.View(google.picker.ViewId.DOCS);
+      view.setMimeTypes(["image/jpeg", "image/jpg", "application/pdf"]);
+      const picker = new google.picker.PickerBuilder()
+          .enableFeature(google.picker.Feature.NAV_HIDDEN)
+          .enableFeature(google.picker.Feature.MULTISELECT_ENABLED)
+          .setAppId(APP_ID)
+          .setOAuthToken(oauthToken)
+          .addView(view)
+          .addView(new google.picker.DocsUploadView())
+          .setDeveloperKey(API_KEY)
+          .setCallback(pickerCallback)
+          .setSize(1051, 650)
+          .build();
+       picker.setVisible(true);
+    }
+  }
+
+  const onPickerApiLoad = () => {
+    setPickerApiLoaded(true);
+    createPicker();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleAuth}>Google Login</button>
     </div>
   );
 }
